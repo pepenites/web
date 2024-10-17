e0ef1fe5c0f60ba960e7e044fc570208ad71fcfd
pepe <pepegarciagb@gmail.com>
2024-10-17 20:47:13 +0200

    hola

---
+++ holamundo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HolaMundo() {
+    return <h1>Hola Mundo!</h1>;
+}
+
+ReactDOM.render(<HolaMundo />, document.getElementById('root'));
